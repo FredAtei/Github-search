a8d830499650296919770937afadaedaf5b8dc98
Fred <akamaatei@gmail.com>
2020-10-05 23:06:01 +0300

    add final sections and fixed bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RoutingModule } from './routing/routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -11,6 +11,8 @@ import { RepoComponent } from './repo/repo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HighlightDirective } from './highlight.directive';
 import { JoinDateCountPipe } from './join-date-count.pipe';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
@@ -24,9 +26,11 @@ import { JoinDateCountPipe } from './join-date-count.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
