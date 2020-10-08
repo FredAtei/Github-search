2592a44fa02cfd62772179cc8e3ed8cdb23d5117
Fred <akamaatei@gmail.com>
2020-10-08 11:26:24 +0300

    add website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { RoutingModule } from './routing/routing.module';
 import { GitSearchService } from './git-search.service';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RepoComponent } from './repo/repo.component';
 import { NgProgressModule } from '@ngx-progressbar/core';
@@ -18,7 +17,6 @@ import { HighlightDirective } from './highlight.directive';
   declarations: [
     AppComponent,
     ProfileComponent,
-    HomeComponent,
     PageNotFoundComponent,
     RepoComponent,
     HighlightDirective
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProfileComponent } from '../profile/profile.component';
-import { HomeComponent } from '../home/home.component';
+import { AppComponent } from '../app.component'
+// import { HomeComponent } from '../home/home.component';
 import { RepoComponent } from '../repo/repo.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
+  { path: "app", component: AppComponent },
+  // { path: "home", component: HomeComponent },
   { path: "profile", component: ProfileComponent },
   { path: "repo", component: RepoComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
